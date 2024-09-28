2d06f69463b77b77099f2351f6f8d87b53dc6b14
shaikat17 <shaikatpal56@gmail.com>
2024-09-28 16:37:03 +0600

    Fix: Fix various bugs on front end

--- client/App.js
+++ client/App.js
@@ -3,11 +3,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import {
   SafeAreaProvider,
 } from 'react-native-safe-area-context';
-
+import { enableScreens } from 'react-native-screens';
 import RootNavigation from './Navigations';
 
 
 export default function App() {
+  enableScreens();
   return (
     <SafeAreaProvider>
       <NavigationContainer>
