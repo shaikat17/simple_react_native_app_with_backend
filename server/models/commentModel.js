0f7a09af2f5b2de8aadf508da7c7d174d9f21c29
shaikat17 <shaikatpal56@gmail.com>
2024-09-29 03:18:17 +0600

    Feat: comment model added.

---
+++ server/models/commentModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post', // Assuming you have a Post model
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // Assuming you have a User model
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+}, {
+  timestamps: true});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+export default Comment
